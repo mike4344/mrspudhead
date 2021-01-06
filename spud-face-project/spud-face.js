3ccfaa4017f4f69ebed2c928b5f483f09b765dda
mike <mikemihalchik@gmail.com>
2021-01-06 16:43:55 -0500

    step1

--- spud-face-project/spud-face.js
+++ spud-face-project/spud-face.js
@@ -1,17 +1,17 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  const title = document.getElementById('title')
-  const firstName = document.getElementById('first-name')
-  const lastName = document.getElementById('last-name')
-  const eyeColor = document.getElementById('eye-color')
-  const height = document.getElementById('height')
   const licenseNum = document.getElementById('license-num')
   const licenseNumConfirm = document.getElementById('license-num-confirm')
   const donorStatus = document.getElementById('donor-status')
   const formAll = document.getElementById('drivers-license-form')
-  // ** Phase 1B: Update license with event delegation and event.target **
   formAll.addEventListener("input", (event) => {
     let value = event.target.value
+    let element = event.target
+    let cardElement = document.getElementById(`card-${element.id}`)
+    
+    cardElement.innerText = value
+
   })
+  // ** Phase 1B: Update license with event delegation and event.target **
 
   // ** Phase 2: Add focus and blur events to form inputs **
 
