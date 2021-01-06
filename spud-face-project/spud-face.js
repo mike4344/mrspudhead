9880fab06044ff7d1ec57c8f19f45f299d2b6036
Jason Leatiota <jasole2007@yahoo.com>
2021-01-06 13:25:17 -0800

    swap

--- spud-face-project/spud-face.js
+++ spud-face-project/spud-face.js
@@ -1,15 +1,25 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-
-  // ** Phase 1B: Update license with event delegation and event.target ** 
-
+  const title = document.getElementById('title')
+  const firstName = document.getElementById('first-name')
+  const lastName = document.getElementById('last-name')
+  const eyeColor = document.getElementById('eye-color')
+  const height = document.getElementById('height')
+  const licenseNum = document.getElementById('license-num')
+  const licenseNumConfirm = document.getElementById('license-num-confirm')
+  const donorStatus = document.getElementById('donor-status')
+  const formAll = document.getElementById('drivers-license-form')
+  // ** Phase 1B: Update license with event delegation and event.target **
+  formAll.addEventListener("input", (event) => {
+    let value = event.target.value
+  })
 
   // ** Phase 2: Add focus and blur events to form inputs **
- 
+
 
   // ** Phase 3: Check that license numbers match **
- 
+
 
   // ** Phase 4: Update submit button click count **
 
- 
+
 });
