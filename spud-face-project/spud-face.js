e2519c2a70ff65bab91d5b4fece97190e1df4dde
mike <mikemihalchik@gmail.com>
2021-01-06 17:31:27 -0500

    phase 4

--- spud-face-project/spud-face.js
+++ spud-face-project/spud-face.js
@@ -3,11 +3,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const licenseNumConfirm = document.getElementById('license-num-confirm')
   const donorStatus = document.getElementById('donor-status')
   const formAll = document.getElementById('drivers-license-form')
-
   formAll.addEventListener("input", (event) => {
     let value = event.target.value
     let element = event.target
     let cardElement = document.getElementById(`card-${element.id}`)
+
     if (element === donorStatus) {
       if (donorStatus.checked) {
         cardElement.innerText = "True"
@@ -15,16 +15,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
         cardElement.innerText = "False"
       }
     } else {
+      cardElement.addEventListener('focus', ()=> cardElement.classList.add('focus'), true)
+      cardElement.addEventListener('blur', ()=> cardElement.classList.remove('focus'), true)
+      // formAll.style = '.focused{backgroundColor:green}'
         cardElement.innerText = value
     }
   })
-  // ** Phase 1B: Update license with event delegation and event.target **
-
-  // ** Phase 2: Add focus and blur events to form inputs **
-
-
   // ** Phase 3: Check that license numbers match **
+licenseNumConfirm.addEventListener('input', event =>{
+  let value2 = event.target.value
+  let value1 = licenseNum.value
 
+  setTimeout(()=>{if (value1 === value2){
+    licenseNum.classList.remove('confirmed')
+    licenseNumConfirm.classList.remove('confirmed')
+  } else{
+    licenseNumConfirm.classList.add('confirmed')
+    licenseNum.classList.add('confirmed')
+  }},3000)
+})
 
   // ** Phase 4: Update submit button click count **
 
