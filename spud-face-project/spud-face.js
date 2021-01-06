228e2f2bb2c91a94a66648be5bd12fb547c55f3b
Jason Leatiota <jasole2007@yahoo.com>
2021-01-06 13:55:33 -0800

    next swap 2

--- spud-face-project/spud-face.js
+++ spud-face-project/spud-face.js
@@ -3,13 +3,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const licenseNumConfirm = document.getElementById('license-num-confirm')
   const donorStatus = document.getElementById('donor-status')
   const formAll = document.getElementById('drivers-license-form')
+
   formAll.addEventListener("input", (event) => {
     let value = event.target.value
     let element = event.target
     let cardElement = document.getElementById(`card-${element.id}`)
-    
-    cardElement.innerText = value
-
+    if (element === donorStatus) {
+      if (donorStatus.checked) {
+        cardElement.innerText = "True"
+      } else {
+        cardElement.innerText = "False"
+      }
+    } else {
+        cardElement.innerText = value
+    }
   })
   // ** Phase 1B: Update license with event delegation and event.target **
 
